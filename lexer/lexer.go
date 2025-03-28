b54fac09c8fc9f611b69346059d22b1cb2804877
Jack Leatherland <jpl92@live.co.uk>
2025-03-28 22:43:11 +0000

    better test structure, finish 1.3

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,6 +30,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.currChar {
 	case '=':
 		tok = newToken(token.ASSIGN, l.currChar)
@@ -50,6 +52,18 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.currChar) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.currChar){
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}else {
+			tok = newToken(token.ILLEGAL, l.currChar)
+		}
 	}
 
 	l.readChar()
@@ -59,3 +73,36 @@ func (l *Lexer) NextToken() token.Token {
 func newToken(tokenType token.TokenType, currChar byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(currChar)}
 }
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.currChar) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace(){
+	for l.currChar == ' ' || l.currChar == '\t' || l.currChar == '\n' || l.currChar == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.currChar){
+		l.readChar()
+	}
+	return l.input[position: l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+
+
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,79 +7,105 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-
-	tests := []struct {
+	type tokenTest struct {
 		expectedType    token.TokenType
 		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
 	}
 
-	tests = append(tests, struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.LET, "let"},
-		{token.IDENT, "five"},
-		{token.ASSIGN, "="},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
-		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
-		{token.COMMA, ","},
-		{token.IDENT, "ten"},
-		{token.RPAREN, ")"},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+	type testCase struct {
+		name           string
+		input          string
+		expectedTokens []tokenTest
+	}
+
+	var tests []testCase
+
+	tests = append(tests, testCase{
+		name:  "Simple symbols",
+		input: `=+(){},;`,
+		expectedTokens: []tokenTest{
+			{token.ASSIGN, "="},
+			{token.PLUS, "+"},
+			{token.LPAREN, "("},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.RBRACE, "}"},
+			{token.COMMA, ","},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		},
 	})
 
-	l := New(input)
+	tests = append(tests, testCase{
+		name: "assignments and functions",
+		input: `let five = 5;
+let ten = 10;
 
-	for i, tt := range tests {
-		tok := l.NextToken()
+let add = fn(x, y) {
+  x + y;
+};
 
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected: %q, got: %q",
-				i, tt.expectedType, tok.Type)
-		}
+let result = add(five, ten);
+`,
+		expectedTokens: []tokenTest{
+			{token.LET, "let"},
+			{token.IDENT, "five"},
+			{token.ASSIGN, "="},
+			{token.INT, "5"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "ten"},
+			{token.ASSIGN, "="},
+			{token.INT, "10"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "add"},
+			{token.ASSIGN, "="},
+			{token.FUNCTION, "fn"},
+			{token.LPAREN, "("},
+			{token.IDENT, "x"},
+			{token.COMMA, ","},
+			{token.IDENT, "y"},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.IDENT, "x"},
+			{token.PLUS, "+"},
+			{token.IDENT, "y"},
+			{token.SEMICOLON, ";"},
+			{token.RBRACE, "}"},
+			{token.SEMICOLON, ";"},
+			{token.LET, "let"},
+			{token.IDENT, "result"},
+			{token.ASSIGN, "="},
+			{token.IDENT, "add"},
+			{token.LPAREN, "("},
+			{token.IDENT, "five"},
+			{token.COMMA, ","},
+			{token.IDENT, "ten"},
+			{token.RPAREN, ")"},
+			{token.SEMICOLON, ";"},
+			{token.EOF, ""},
+		}})
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected: %q, got: %q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			lexer := New(tt.input)
+			for i, expected := range tt.expectedTokens {
+				tok := lexer.NextToken()
+
+				if tok.Type != expected.expectedType {
+					t.Fatalf("test %q, token[%d] - expected type: %q, got: %q",
+						tt.name, i, expected.expectedType, tok.Type)
+				}
+
+				if tok.Literal != expected.expectedLiteral {
+					t.Fatalf("test %q, token[%d] - expected literal: %q, got: %q",
+						tt.name, i, expected.expectedLiteral, tok.Literal)
+				}
+
+			}
+		})
 	}
 }
